1df5c6bdc9d884756af966b3c8a0e7d893326783
mcstrother <mcstrother@localhost>
2010-04-04 22:25:22 -0500

    filled in SmoothnessLossFunction

--- tieredscene/SmoothnessLossFunction.py
+++ tieredscene/SmoothnessLossFunction.py
@@ -6,7 +6,22 @@ Created on Apr 4, 2010
 
 
 class SmoothnessLossFunction(object):
+    _label_set = None
+    
+    def __init__(self):
+        if self._label_set is None:
+            raise NotImplementedError("Subclasses of SmoothnessLossFunction must define a '_label_set' class variable.")
     
     def __call__(self, pixel1, label1, pixel2, label2):
         """
         """
+        if (pixel1.column == pixel2.column-1) and pixel1.row == pixel2.row:
+            self.horizontal_loss(pixel1, label1, pixel2, label2)
+        elif (pixel1.row == pixel2.row-1) and pixel1.column == pixel2.column:
+            self.vertical_loss(pixel1, label1, pixel2, label2)
+            
+    def vertical_loss(self, pixel1, label1, pixel2, label2):
+        raise NotImplementedError()
+    
+    def horizontal_loss(self, pixel1, label1, pixel2, label2):
+        raise NotImplementedError()
