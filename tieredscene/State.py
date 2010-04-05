a1daa54a1ae2eebae6bd43c614b084fbd811db7d
mcstrother <mcstrother@localhost>
2010-04-04 22:33:27 -0500

    added _label_set to State

--- tieredscene/State.py
+++ tieredscene/State.py
@@ -7,7 +7,11 @@ Created on Apr 4, 2010
 
 class State(object):
     
-    def __init__(self):
-        self._i # T-M boundary
-        self._j # M-B boundary
-        self._mlabel # the label of the middle region
+    def __init__(self, label_set, i, j, mlabel):
+        self._label_set = label_set
+        self._i = i # T-M boundary
+        self._j = j# M-B boundary
+        self._mlabel = mlabel # the label of the middle region
+        
+        if not (mlabel in self._label_set.middle):
+            raise ValueError("mlabel " + str(mlabel) + "not found in label_set")
