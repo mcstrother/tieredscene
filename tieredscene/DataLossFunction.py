cfcecc1ceea6fb521ce251c9486c36e2ddc5e914
mcstrother <mcstrother@localhost>
2010-04-15 22:58:34 -0500

    documentation

--- tieredscene/DataLossFunction.py
+++ tieredscene/DataLossFunction.py
@@ -6,6 +6,20 @@ Created on Apr 4, 2010
 
 
 class DataLossFunction(object):
+    """
+    An object representing the data loss function
+    being used for a run of the tiered scene labeling
+    algorithm.
+    
+    A data loss function can be any function which, given
+    a pixel in an image and a label assigned to it, returns
+    a numerical loss which is incurred by assigning that
+    label to that pixel.  The data loss function is only
+    allowed to take into account the pixel's x-y coordinates
+    in the image and the pixel's intensity.  In contrast, 
+    a smoothness loss function may take into account the 
+    intensities and labels of neighboring pixels.
+    """
     
     def __call__(self, pixel, label):
         """
