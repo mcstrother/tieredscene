91a11407bbd7a33965b33f0ad6879aeca51a4122
mcstrother <mcstrother@localhost>
2010-04-27 12:32:22 -0500

    separated _FTables from LossTable.  removed unnecessary data attributes from all objects

--- tieredscene/LossTable.py
+++ tieredscene/LossTable.py
@@ -20,63 +20,24 @@ class _UTable(object):
 
 
 
-
-
-
-class LossTable(object):
-    '''
-    Maintains a dynamic programming table of dimension
-    (# of possible states, # of columns in the image).
-    Once initialized, cell a,b of the table represents
-    the loss of the optimal column-state mapping if the
-    image consists of only columns 0 through b and
-    column b is assign the state corresponding to the
-    int a. 
+class _FTables(object):
     
-     
-    '''
-
-
-    def __init__(self, image_array, data_loss_func, smoothness_loss_func):
-        '''
-        Constructor
-        '''
-        if not ( data_loss_func.label_set() == smoothness_loss_func.label_set() ):
-            raise ValueError('data_loss_func and smoothness_loss_func must operate over the same label set')
-        self._label_set = data_loss_func.label_set
-        self._image_array = image_array
-        self._hslc = HorizontalSmoothnessLossCache(self._image_array, smoothness_loss_func)
-        self._u = _UTable(image_array, data_loss_func, smoothness_loss_func)
-        #initialize the table and fill in the first column
-        self._table = numpy.zeros((State.count_states(image_array, self._label_set), image_array.shape[1]  ) )
-        self._table[:, 0] = [self._U(State.from_int(i, self._label_set, self._image_array), 0) for i in range(self._table.shape[0] ) ] #init the first column of the table
-        self._trace = numpy.zeros((State.count_states(image_array, self._label_set), image_array.shape[1]  ) ) # the number in each cell corresponds to the optimal state number for the preceding column
-        n = self._image_array.shape[0]
-        
-        for column in xrange(1, self._table.shape[1]):
-            for this_label in self._label_set.all_labels:
-                for previous_label in self._label_set.all_labels:
-                    fs = self._get_Fs(column, previous_label, this_label)
-                    for i in xrange(n):
-                        for j in xrange(i-1, n):
-                            curr_state = State(i, j, this_label, self._label_set, self._image_array)
-                            best_prev_state= None
-                            best_prev_state_value = None
-                            for rel_pos in xrange(6):
-                                f_out = fs[rel_pos](i,j)
-                                curr_value = f_out[0]
-                                if best_prev_state is None or curr_value < best_prev_state_value:
-                                    ib = f_out[1]
-                                    jb = f_out[2]
-                                    best_prev_state = State(ib, jb, previous_label, self._label_set, self._image_array)
-                                    best_prev_state_value = curr_value
-                            self._table[curr_state.as_int(), column] = best_prev_state_value + self._u.get_loss(curr_state, column)
-                            self._trace[curr_state.as_int(), column] = best_prev_state
-        
+    def __init__(self, hslc, label_set, image_array, loss_table_column, col_num, previous_label, this_label ):
+        n = image_array.shape[0]
+        image_array = image_array
+        label_set = label_set
+        self._fs = [None] *6
+        for relative_positioning in xrange(6):
+            I, J, Ib, Jb, C = self._get_decoupled_functions(hslc, n, label_set, col_num, previous_label, this_label, relative_positioning)
+            Ep = self._get_E_prime(n, label_set, image_array, loss_table_column, col_num, previous_label, Ib)
+            h = self._get_h(n, Ep, Jb)
+            self._fs[relative_positioning] = lambda i,j : (I(i) + J(j) + C() + h(i,j)[0], h(i,j)[2], h(i,j)[1])
         
     
-    
-    def _get_E_prime(self, col, previous_label, Ib):
+    def get_best_i_j(self, i, j, relative_positioning):
+        return self._fs[relative_positioning](i,j)
+
+    def _get_E_prime(self, n, label_set, image_array, loss_table_column,  col, previous_label, Ib):
         """Calculate E' in O(n^2) time as described in the Felzenszwalb paper
         
         Runs in O(n^2) time.
@@ -93,19 +54,14 @@ class LossTable(object):
             The first value in the tuple is the min over all ib <= i of E[ib, jb] + Ib(ib).
             The second value in the tuple is the value of ib that yields the min.
         """
-        #ln1 = self._label_set.label_to_int(previous_label)
-        #ln2 = self._label_set.label_to_int(this_label)
-        n = self._image_array.shape[0]
-        
-        
         ep_table = [None] * n
         ep_ind_table = [None] * n
         for jb in xrange(n):
             min_ind_arr = numpy.empty(jb+1)
             min_val_arr = numpy.empty(jb+1)
             for ib in xrange(jb+1):
-                previous_state = State(ib, jb, previous_label, self._label_set, self._image_array)
-                curr_val = self._table[previous_state.as_int(), col-1 ] + Ib(ib)
+                previous_state = State(ib, jb, previous_label, label_set, image_array)
+                curr_val = loss_table_column[previous_state.as_int()] + Ib(ib)
                 if (ib == 0) or curr_val < min_val_arr[ib-1]:
                     min_val_arr[ib] = curr_val
                     min_ind_arr[ib] = ib
@@ -118,7 +74,7 @@ class LossTable(object):
         return Ep
         
     
-    def _get_h(self, Ep, Jb):
+    def _get_h(self,n, Ep, Jb):
         """Calculate h as described in equation 22 of the Felzenszwalb paper
         
         Runs in O(n^2) time.
@@ -135,7 +91,6 @@ class LossTable(object):
             The second value is the value of jb that yields the min.
             The third value is the value of ib that yields the min for the given jb (as returned by the Ep function)
         """
-        n = self._image_arr.shape[0]
         h_table = [None] * n
         h_ind_table = [None]*n
         ep_ind_table = [None]*n
@@ -154,16 +109,8 @@ class LossTable(object):
         h = lambda i, j: (h_table[i][j-i], h_ind_table[i][j-i], ep_ind_table[i][j-i])
         return h
         
-    def _get_Fs(self, column, previous_label, this_label, relative_positioning):
-        out = [None] *6
-        for relative_positioning in xrange(6):
-            I, J, Ib, Jb, C = self._get_decoupled_functions(self._hslc, column, previous_label, this_label, relative_positioning)
-            Ep = self._get_E_prime(column, previous_label, Ib)
-            h = self._get_h(Ep, Jb)
-            out[relative_positioning] = lambda i,j : (I(i) + J(j) + C() + h(i,j)[0], h(i,j)[2], h(i,j)[1])
-        return out
     
-    def _get_decoupled_functions(self, hslc, col, previous_label, this_label, positioning ):  #TODO: this is an awful method name
+    def _get_decoupled_functions(self, hslc, n, label_set, col, previous_label, this_label, positioning ):  #TODO: this is an awful method name
         """The horizontal loss between `column` and the previous column broken down into functions of i, j, ib, and jb
         
         A key observation of the Felzenszwalb paper is that if we fix the state of
@@ -199,11 +146,10 @@ class LossTable(object):
         5. ib, jb, i, j
         """
         table = hslc.table
-        ln1 = self._label_set.label_to_int(previous_label)
-        ln2 = self._label_set.label_to_int(this_label)
-        n = self._image_array.shape[0]
-        t = self._label_set.label_to_int(self._label_set.top)
-        b = self._label_set.label_to_int(self._label_set.bottom)
+        ln1 = label_set.label_to_int(previous_label)
+        ln2 = label_set.label_to_int(this_label)
+        t = label_set.label_to_int(label_set.top)
+        b = label_set.label_to_int(label_set.bottom)
         
         
         C = lambda x: table[n, col, ln1, ln2] 
@@ -240,16 +186,58 @@ class LossTable(object):
         else:
             raise Exception("Invalid positioning.  Expected a number in range(6), but got " + str(positioning))
         return (I,J,Ib,Jb,C)
-            
 
 
-class _FTable(object):
+class LossTable(object):
+    '''
+    Maintains a dynamic programming table of dimension
+    (# of possible states, # of columns in the image).
+    Once initialized, cell a,b of the table represents
+    the loss of the optimal column-state mapping if the
+    image consists of only columns 0 through b and
+    column b is assign the state corresponding to the
+    int a. 
     
-    def __init__(self):
-        pass
-    
-    def get_best_prev_i_j(self, relative_positioning):
-    
+     
+    '''
+
+
+    def __init__(self, image_array, data_loss_func, smoothness_loss_func):
+        '''
+        Constructor
+        '''
+        if not ( data_loss_func.label_set() == smoothness_loss_func.label_set() ):
+            raise ValueError('data_loss_func and smoothness_loss_func must operate over the same label set')
+        label_set = data_loss_func.label_set
+        image_array = image_array
+        hslc = HorizontalSmoothnessLossCache(image_array, smoothness_loss_func)
+        u = _UTable(image_array, data_loss_func, smoothness_loss_func)
+        #initialize the table and fill in the first column
+        self._table = numpy.zeros((State.count_states(image_array, label_set), image_array.shape[1]  ) )
+        self._table[:, 0] = [u.get_loss(State.from_int(i, label_set, image_array), 0) for i in range(self._table.shape[0] ) ] #init the first column of the table
+        self._trace = numpy.zeros((State.count_states(image_array, label_set), image_array.shape[1]  ) ) # the number in each cell corresponds to the optimal state number for the preceding column
+        n = image_array.shape[0]
+        
+        for column in xrange(1, self._table.shape[1]):
+            for this_label in label_set.all_labels:
+                for previous_label in label_set.all_labels:
+                    ftables = _FTables(hslc, label_set, image_array, self._table[:,column-1], column, previous_label, this_label)
+                    for i in xrange(n):
+                        for j in xrange(i-1, n):
+                            curr_state = State(i, j, this_label, label_set, image_array)
+                            best_prev_state= None
+                            best_prev_state_value = None
+                            for rel_pos in xrange(6):
+                                f_out = ftables.get_best_i_j(i, j, rel_pos)
+                                curr_value = f_out[0]
+                                if best_prev_state is None or curr_value < best_prev_state_value:
+                                    ib = f_out[1]
+                                    jb = f_out[2]
+                                    best_prev_state = State(ib, jb, previous_label, label_set, image_array)
+                                    best_prev_state_value = curr_value
+                            self._table[curr_state.as_int(), column] = best_prev_state_value + u.get_loss(curr_state, column)
+                            self._trace[curr_state.as_int(), column] = best_prev_state
+        
     
 
         
