ea8e855b21e1a580f98b8f42daadac30023a9e3d
mcstrother <mcstrother@localhost>
2010-04-27 14:27:07 -0500

    added utilities.py with running_min function

---
+++ tieredscene/utilities.py
@@ -0,0 +1,33 @@
+'''
+Created on Apr 27, 2010
+
+@author: mcstrother
+'''
+
+import numpy
+
+def running_min(arr):
+    """
+    Parameters
+    ----------
+    arr : a 1d numpy array
+    
+    Returns
+    -------
+    min_arr : the running min of the input
+    min_ind_arr: the running argmin of the input
+    """
+    min_arr = numpy.empty(arr.shape)
+    min_ind_arr = numpy.empty(arr.shape) 
+    min_arr[0] = arr[0]
+    min_ind_arr[0] = 0
+    for i in xrange(1, arr.shape[0]):
+        if arr[i] < min_arr[i-1]:
+            min_arr[i] = arr[i]
+            min_ind_arr[i] = i
+        else:
+            min_arr[i] =  min_arr[i-1]
+            min_ind_arr[i] = min_ind_arr[i-1]
+            
+    return (min_arr, min_ind_arr)
+    
