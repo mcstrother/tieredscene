5a3888d6b3e26acf9d4cbbbcb166c0eff6722f6e
mcstrother <mcstrother@localhost>
2010-04-28 01:37:45 -0500

    filled in main.py

--- tieredscene/Segmentation.py
+++ tieredscene/Segmentation.py
@@ -9,9 +9,8 @@ import numpy
 
 class Segmentation(object):
     
-    def __init__(self, image, data_loss_func, smoothness_loss_func):
-        im = image.convert('L') 
-        loss_table = LossTable(numpy.array(im), data_loss_func, smoothness_loss_func )
+    def __init__(self, image_array, data_loss_func, smoothness_loss_func):
+        loss_table = LossTable(image_array, data_loss_func, smoothness_loss_func )
         self._state_list = loss_table.get_optimal_state_list()
         
     def to_array(self):
--- tieredscene/main.py
+++ tieredscene/main.py
@@ -5,16 +5,22 @@ Created on Apr 4, 2010
 '''
 import sys
 import optparse
-
+import Image
+from tieredscene import GeometricClassLabeling, Segmentation
+import numpy
 
 def main(argv):
     usage = "%prog [options] FILE"
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option('-m', '--middle-regions', action='store', type='int', dest='middle_regions',
-                      help='the number of regions the middle section of the image should be partitioned into. default is 3', default=3 )
+    #parser.add_option('-m', '--middle-regions', action='store', type='int', dest='middle_regions',
+    #                  help='the number of regions the middle section of the image should be partitioned into. default is 3', default=3 )
     (options, args) = parser.parse_args(argv)
+    image_file_name = args[0]
+    image_array = numpy.array(Image.open(image_file_name, 'L'))
+    data_loss_function = GeometricClassLabeling.GCLDataLossFunction()
+    smoothness_loss_function = GeometricClassLabeling.GCLSmoothnessLossFunction(image_array)
+    segmentation = Segmentation.Segmentation(image_array, data_loss_function, smoothness_loss_function)
     
-
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
