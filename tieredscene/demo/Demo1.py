06654edbc33452805496c8095a42946e6d93c5f2
mcstrother <mcstrother@localhost>
2010-05-04 00:57:07 -0500

    changed Demo1 to save an output png file

--- tieredscene/demo/Demo1.py
+++ tieredscene/demo/Demo1.py
@@ -6,5 +6,5 @@ Created on Apr 28, 2010
 from tieredscene import main
 
 if __name__=='__main__':
-    argv = '-o demo1_out.jpg ../test/testimage_tiny.png'.split()
+    argv = '-o demo1_out.png ../test/testimage_tiny.png'.split()
     main.main(argv)
